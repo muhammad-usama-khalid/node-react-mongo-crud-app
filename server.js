49dd715e446f10be2bf8f38b64e4c58d2aaadba1
muhammad-usama-khalid <muhammadusama1004@gmail.com>
2023-09-10 21:00:25 +0500

    file structure changed

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require('dotenv').config()
 
 const app = express();
+const path = `${__dirname}\\.\\client-app\\build`;
+app.use(express.static(path));
 
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -29,6 +32,10 @@ db.mongoose
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
+
+app.get('/', function (req, res) {
+  res.sendFile(path + "index.html");
+});
 
 require("./app/routes/tutorial.routes")(app);
 
